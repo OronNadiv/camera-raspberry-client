c06f4f9330de42b1b2673fc5952051a260aef0f0
Oron Nadiv <oron@nadiv.us>
2016-09-13 23:45:07 -0700

    updating dependencies

--- scripts/camera.sh
+++ scripts/camera.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # This file starts the raspberry pi client.
 # Follow installation instructions in the README.md file.
 # Then place this file in the user's root directory.
@@ -6,9 +8,9 @@ cd ~/camera-raspberry-client
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 nvm use
-export PRIVATE_KEY=<PASTE GENERATED PRIVATE KEY>
+export PRIVATE_KEY= # PASTE GENERATED PRIVATE KEY
 export NODE_ENV=production
-export LOGIN_URL=https://<LOGIN_SERVER_URL>
-export PUSH_URL=https://<PUSH_SERVER_URL>
-export STORAGE_URL=https://<STORAGE_SERVER_URL>
+export LOGIN_URL= # https://<LOGIN_SERVER_URL>
+export PUSH_URL= # https://<PUSH_SERVER_URL>
+export STORAGE_URL= # https://<STORAGE_SERVER_URL>
 npm start
